1bfe17d2866fbf389e9140772579a11c77c918ce
Mike McGuire <mcguire@mullenhigh.com>
2016-01-06 09:16:51 -0700

    end of class 1

--- weatherApp.js
+++ weatherApp.js
@@ -1,1 +1,15 @@
 var myApp = angular.module('weatherApp', []);
+
+myApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) 
+    {
+    $scope.city = cityService.city;
+    $scope.$watch('city', function() 
+        {
+        cityService.city = $scope.city;
+        })
+    }]);
+                                    
+myApp.service('cityService', function()
+    {
+    this.city = "Denver, CO";
+    })                                  
